a5c41f3228696fe82f345cbcf3cbcb2c62a492ae
user-Basement <31741492+user-Basement@users.noreply.github.com>
2017-09-07 17:04:51 -0500

    Create injectGrid.js

---
+++ grid/injectGrid.js
@@ -0,0 +1,40 @@
+document.write(
+  '<g class="secondary">' + '\n' +
+  '  <g class="h-lns" transform="translate( 0 -4 )">' + '\n' + 
+  '	<line x1="9.5" x2="90" y1="50" y2="50" />' + '\n' +
+  '  </g>' + '\n' +
+  '  <g class="v-lns" transform="translate( -4 0 )">' + '\n' +
+  '	<line x1="50" x2="50" y1="9.5" y2="90" />' + '\n' +
+  '  </g>' + '\n' +
+  '</g>' + '\n' +
+  '<g class="primary"> <g class="h-lns"></g> <g class="v-lns"></g> </g>'
+  + '\n' +
+  '<g class="cntr-lns">' + '\n' +
+  '  <line x1="9.5" x2="90" y1="50" y2="50" />' + '\n' +
+  '  <line x1="50" x2="50" y1="9.5" y2="90" />' + '\n' +
+  '</g>' + '\n' +
+  '<g class="rule-lns">' + '\n' +
+  '  <line x1="9.5" y1="9.75" x2="90" y2="9.75" />' + '\n' +
+  '  <line x1="9.75" y1="9.5" x2="9.75" y2="90" />' + '\n' +
+  '</g>' + '\n' +
+  '<g class="label">' + '\n' +
+  '  <text x="9.75" y="9.75"' + '\n' +
+  '		alignment-baseline="start" text-anchor="end"' + '\n' +
+  '  >0</text>       ' + '\n' +
+  '  <g class="h-num" transform="translate( 0 0.25 )"></g>' + '\n' +
+  '  <g class="v-num" transform="translate( 0.25 0 )"></g>' + '\n' +
+  '  <g class="letters">' + '\n' +
+  '	<text x="94" y="9.75"' + '\n' +
+  '    alignment-baseline="middle" text-anchor="middle"' + '\n' +
+  '	>x</text>' + '\n' +
+  '	<text x="9.5" y="94.15"' + '\n' +
+  '		  alignment-baseline="middle" text-anchor="middle"' + '\n' +
+  '	>y</text>' + '\n' +
+  '  </g>' + '\n' +
+  '<text ' + '\n' +
+  '  x="50" y="95" ' + '\n' +
+  '  text-anchor="middle"' + '\n' +
+  '  class="title" ' + '\n' +
+  '>SVG GRID</text>' + '\n' +
+  '</g>'
+);      
